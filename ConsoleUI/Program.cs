a2b5c52a3a7461d72dd6f0f2ae086fec7a958c08
mustafa <mustafa@DESKTOP-ITHCNH9>
2023-02-27 09:53:12 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,24 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+using System;
+
+
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ProductManager productManager = new ProductManager(new EfProductDal());
+
+            foreach (var product in productManager.GetByUnityPrice(max:200,min:45))
+            {
+                Console.WriteLine(product.ProductName);
+            }
+            
+        }
+    }
+}
